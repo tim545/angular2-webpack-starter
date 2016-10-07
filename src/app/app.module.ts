2ebb9e5901dd695dd530d6eeeb102041d0105c45
Tim <tim.freedman@live.com.au>
2016-10-07 22:20:11 +1100

    fix imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import { TranslateModule } from 'ng2-translate';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
+import { TranslateModule } from 'ng2-translate/ng2-translate';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
